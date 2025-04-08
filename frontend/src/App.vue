ccf184695c14fe862204cb1e0cb1102d827dfe71
Pheng Menghour <phengmenghour@Menghour-Pheng.local>
2025-04-08 21:53:29 +0700

    Finish Authentication page Forgot password page SignIn page SignUp page Responsive is not working good yet

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,14 @@
 import datePicker from '../src/components/datePicker.vue';
 import SettingPage from '../src/views/SettingPage.vue';
 import SettingDetailPage from './views/SettingDetailPage.vue';
+import AuthenticationPage from './views/AuthenticationPage.vue';
 </script>
 
 <template>
   <!-- <datePicker/> -->
-   <!-- <SettingPage/> -->
-    <SettingDetailPage/>
+  <!-- <SettingPage/> -->
+  <!-- <SettingDetailPage/> -->
+  <AuthenticationPage />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ frontend/src/views/AuthenticationPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="auth-container">
+      <SignIn v-if="currentView === 'signIn'" @switch="switchView" />
+      <SignUp v-if="currentView === 'signUp'" @switch="switchView" />
+      <ForgotPassword v-if="currentView === 'forgotPassword'" @switch="switchView" />
+    </div>
+  </template>
+  
+  <script>
+  import SignIn from '@/components/Authentication/SignIn.vue'
+  import SignUp from '@/components/Authentication/SignUp.vue'
+  import ForgotPassword from '@/components/Authentication/ForgotPassword.vue'
+  
+  export default {
+    components: {
+      SignIn,
+      SignUp,
+      ForgotPassword
+    },
+    data() {
+      return {
+        currentView: 'signIn' // default view
+      }
+    },
+    methods: {
+      switchView(view) {
+        this.currentView = view
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .auth-container {
+    min-height: 100vh;
+    background: #f5f5f5;
+  }
+  </style>
+  
