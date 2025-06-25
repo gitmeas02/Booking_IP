604880c3e7aa00d3a455e4d19de2723a1e4f5e14
gitmeas02 <measreaksmey02@gmail.com>
2025-06-25 16:32:54 +0700

    ji

--- frontend/src/views/SettingPage.vue
+++ frontend/src/views/SettingPage.vue
@@ -111,12 +111,6 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.dashboard {
-  padding: 40px;
-  background-color: #f4f4f4;
-  font-family: sans-serif;
-  min-height: 100vh;
-}
 
 .profile {
   display: flex;
